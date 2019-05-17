5e9cb642d2f5b83d0e6efd8797ff439acf5d4efe
Thiago Ferronatto <thiagoferronatto18@gmail.com>
2019-05-17 19:54:52 -0400

    Outro commit

--- app/src/main/java/com/tcc/reuniapp/CompromissoAdapter.java
+++ app/src/main/java/com/tcc/reuniapp/CompromissoAdapter.java
@@ -1,5 +1,6 @@
 package com.tcc.reuniapp;
 
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.graphics.Color;
@@ -16,15 +17,15 @@ import android.widget.TextView;
 import java.util.List;
 
 public class CompromissoAdapter extends RecyclerView.Adapter<CompromissoAdapter.CompromissoViewHolder> {
-  List<Compromisso> compromissos;
-  Context context;
+  private List<Compromisso> compromissos;
+  private Context context;
 
   CompromissoAdapter(List<Compromisso> compromissos, Context context) {
     this.context = context;
     this.compromissos = compromissos;
   }
 
-  public static class CompromissoViewHolder extends RecyclerView.ViewHolder {
+  static class CompromissoViewHolder extends RecyclerView.ViewHolder {
     TextView nome, data, horario;
     CardView cardView;
 
@@ -36,20 +37,18 @@ public class CompromissoAdapter extends RecyclerView.Adapter<CompromissoAdapter.
       cardView = itemView.findViewById(R.id.cardview);
     }
   }
-
+  @NonNull
   @Override
-  public CompromissoViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+  public CompromissoViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
     View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item, parent, false);
-    CompromissoViewHolder cvh = new CompromissoViewHolder(v);
-    return cvh;
+    return new CompromissoViewHolder(v);
   }
 
   @Override
-  public void onBindViewHolder(@NonNull CompromissoViewHolder viewHolder, final int i) {
+  public void onBindViewHolder(@NonNull CompromissoViewHolder viewHolder, @SuppressLint("RecyclerView") final int i) {
     viewHolder.nome.setText(compromissos.get(i).getNome());
     viewHolder.data.setText(compromissos.get(i).getData());
     viewHolder.horario.setText(compromissos.get(i).getHorario());
-    Log.i("cor", compromissos.get(i).getCor());
     if (compromissos.get(i).getCor().equals("vermelho")) {
       viewHolder.cardView.setCardBackgroundColor(Color.rgb(255, 100, 100));
     }
